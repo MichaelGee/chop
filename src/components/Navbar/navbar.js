bc4b5e885b2e044664cd928d5a3e79501569ae6f
Michael <michaelgatesjr@gmail.com>
2020-05-12 09:05:41 +0100

    done with the bannner

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,10 +3,6 @@ import React from "react";
 const Navbar = () => {
   /* const [open, setOpen] = React.useEffect(false); */
 
-  const handleClick = (e) => {
-    document.getElementsByClassName(".menu").style.display = "none";
-  };
-
   return (
     <div>
       <div className='curved-banner'>
@@ -14,7 +10,7 @@ const Navbar = () => {
           <div className='flex items-center flex-shrink-0 text-white mr-6'>
             <span className='font-semibold text-3xl tracking-tight'>Chop</span>
           </div>
-          <div className='block lg:hidden' onClick={handleClick}>
+          <div className='block lg:hidden'>
             <button className='flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white'>
               <svg
                 className='fill-current h-3 w-3'
@@ -27,7 +23,8 @@ const Navbar = () => {
             </button>
           </div>
           <div className='menu w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
-            <div className='text-sm lg:flex-grow'>
+            <div className='text-sm lg:flex-grow'></div>
+            <div>
               <a
                 href='#responsive-header'
                 className='block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4'
@@ -47,16 +44,29 @@ const Navbar = () => {
                 Sign Up
               </a>
             </div>
-            <div>
-              <a
-                href='#'
-                className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0'
-              >
-                Download
-              </a>
-            </div>
           </div>
         </nav>
+        <div className='banner-text'>
+          <h1 className='text-4xl font-thin text-center'>
+            What you cookin' ?
+            <br />
+            Share it with everyone!
+          </h1>
+          <div className='buttons flex justify-between my-3 space-x-3'>
+            <a
+              href='#'
+              className='inline-block text-sm px-4 py-2 leading-none border rounded text-teal-500 hover:border-white border-transparent hover:text-white hover:bg-transparent bg-white mt-4 px-8 lg:mt-0'
+            >
+              Log In
+            </a>
+            <a
+              href='#'
+              className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 px-8 lg:mt-0'
+            >
+              Sign In
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
